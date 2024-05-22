172aafb3edf0c12f851c3bf8e71ec51060c7989d
DiegoTriana <diegotriana2005@gmail.com>
2024-05-21 22:46:55 -0500

    cambio1

---
+++ src/main/java/Classes/Inmobiliario.java
@@ -0,0 +1,33 @@
+
+package Classes;
+
+
+public class Inmobiliario {
+    
+    private Jugador owner;
+    private String type;
+
+    public Inmobiliario(Jugador owner, String type) {
+        this.owner = owner;
+        this.type = type;
+    }
+
+    public Inmobiliario() {
+    }
+
+    public Jugador getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Jugador owner) {
+        this.owner = owner;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
